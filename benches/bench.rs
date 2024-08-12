ba41b700966436477adbb511a7fb5adf3c3047b4
Martin Siklosi <martin@siklosi.se>
2024-08-12 16:33:52 +0200

    added benching

---
+++ benches/bench.rs
@@ -0,0 +1,16 @@
+#![feature(test)]
+
+extern crate test;
+
+#[cfg(test)]
+mod tests {
+	use countdown_numbers_rs::find_combination;
+	use test::Bencher;
+
+	#[bench]
+	fn bench_find_combinations(b: &mut Bencher) {
+		let numbers = vec![25, 50, 75, 100, 3, 6];
+		let target = 952;
+		b.iter(|| find_combination(&numbers, &target));
+	}
+}
